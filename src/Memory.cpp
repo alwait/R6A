2302f8580bca6b0efad64459eb7ab8dd5bc12d63
alwait <aleksander.czekaj27@gmail.com>
2024-10-10 17:35:40 +0200

    more files added

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -3,60 +3,62 @@
 Memory::Memory():file(){
 }
 
-void Memory::format(){
+void Memory::format(const char* fileName, int dataSize, int savesSize){
     SPIFFS.format();
-    file = SPIFFS.open(FILE_NAME, "wb");
+    file = SPIFFS.open(fileName, "wb");
     if (!file) {Serial.println("Failed to create file"); return;}
 
-    vector<float> emptyData(NUM_OF_AXIS*128, 0.);
-    file.write((const uint8_t*)emptyData.data(), NUM_OF_AXIS * sizeof(float) *128); 
+    vector<float> emptyData(dataSize*savesSize, 0.);
+    file.write((const uint8_t*)emptyData.data(), dataSize * sizeof(float) *savesSize); 
     file.close(); 
 }
 
 
 void Memory::mount(){
+    //format(FILE_NAME, NUM_OF_AXIS, MAX_SIZE_POSITIONS);
+    //format(FILE_NAME_2, MAX_STAGES_SIZE, MAX_SIZE_POSITIONS);
     if (!SPIFFS.begin(true)) {
         Serial.println("SPIFFS falut");
     } else Serial.println("SPIFFS OK");
 }
 
-void Memory::writeVector(int index, const vector<float>& data){
+void Memory::writeVector(const char* fileName, int dataSize, int index, const vector<float>& data){
     if(index<0){ Serial.println("Index err"); return;}
-    if(data.size() != NUM_OF_AXIS){ Serial.println("Size not correct"); return;}
-    file = SPIFFS.open(FILE_NAME, "r+b");
+    if(data.size() != dataSize){ Serial.println("Size not correct"); return;}
+    file = SPIFFS.open(fileName, "r+b");
     if(!file){ Serial.println("File open err"); return;}
 
-    int dataPosition = index * NUM_OF_AXIS * sizeof(float);
+    int dataPosition = index * dataSize * sizeof(float);
     file.seek(dataPosition);
-    file.write((const uint8_t*)data.data(), NUM_OF_AXIS * sizeof(float));
+    file.write((const uint8_t*)data.data(), dataSize * sizeof(float));
     file.close();
 
     Serial.print("Data saved ");
     Serial.print(index);
     Serial.println(":");
-    for (size_t i = 0; i < NUM_OF_AXIS; i++){
+    for (size_t i = 0; i < dataSize; i++){
         Serial.print(data.at(i));
         Serial.print(";");
     }
     Serial.println("");
 }
 
-vector<float> Memory::readVector(int index){
+vector<float> Memory::readVector(const char* fileName, int dataSize, int index){
     if(index<0){ Serial.println("Index err"); return vector<float>(1,{0});}
-    vector<float> data(NUM_OF_AXIS);
+    vector<float> data(dataSize);
 
-    file = SPIFFS.open(FILE_NAME, "rb");
+    file = SPIFFS.open(fileName, "rb");
     if(!file){ return data; Serial.println("Read failed");}
 
-    int dataPosition = index * NUM_OF_AXIS * sizeof(float);
+    int dataPosition = index * dataSize * sizeof(float);
     file.seek(dataPosition);
-    file.read((uint8_t*)data.data(), NUM_OF_AXIS * sizeof(float));
+    file.read((uint8_t*)data.data(), dataSize * sizeof(float));
     file.close();
     
     Serial.print("Data read ");
     Serial.print(index);
     Serial.println(":");
-    for (size_t i = 0; i < NUM_OF_AXIS; i++){
+    for (size_t i = 0; i < dataSize; i++){
         Serial.print(data.at(i));
         Serial.print(";");
     }
--- src/Memory.h
+++ src/Memory.h
@@ -2,6 +2,10 @@
 #define MEMORY_H
 
 #define FILE_NAME "/positions.bin"
+#define FILE_NAME_2 "/stages.bin"
+#define STAGES_SIZE 32
+#define MAX_SIZE_POSITIONS 128
+#define MAX_SIZE_STAGES 24
 
 #include "SPIFFS.h"
 #include "Arduino.h"
@@ -16,9 +20,9 @@ private:
 public:
     Memory();
     void mount();
-    void format();
-    void writeVector(int, const vector<float>&);
-    vector<float> readVector(int);
+    void format(const char*, int, int);
+    void writeVector(const char* , int , int, const vector<float>&);
+    vector<float> readVector(const char* , int ,int);
 };
 
 #endif // MEMORY
